9affdd72aebf13e31088444c22b025e0108206e1
LeeJihun <ljh09060@gmail.com>
2025-06-08 08:56:34 +0900

    feat: Update SetString method to use const char* for better safety

--- 03/RacingCarClassBase.cpp
+++ 03/RacingCarClassBase.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <cstring>
-#include "car.h"
+
 using namespace std;
+
+namespace CAR_CONST {
+	enum {
+		ID_LEN = 20,
+		MAX_SPD = 200,
+		FUEL_STEP = 2,
+		ACC_STEP = 10,
+		BRK_STEP = 10
+	};
+}
 
 class Car {
 private:
@@ -9,13 +19,13 @@ private:
 	int fuelGauge;
 	int curSpeed;
 public:
-	void InitMembers(char *ID, int fule);
+	void InitMembers(const char *ID, int fule);
 	void ShowCarState();
 	void Accel();
 	void Break();
 };
 
-void Car::InitMembers(char *ID, int fuel){
+void Car::InitMembers(const char *ID, int fuel){
 	strcpy(gamerID, ID);
 	fuelGauge = fuel;
 	curSpeed=0;
@@ -54,7 +64,7 @@ void Car::Break() {
 
 int main(void) {
 	Car run99;
-	run99.InitMembers((char *)"run99", 100);
+	run99.InitMembers("run99", 100);
 	run99.Accel();
 	run99.ShowCarState();
 	run99.Break();
