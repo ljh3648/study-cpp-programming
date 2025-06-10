de56272dbe4e64d02f3806d84ef81d29e4ff6db9
LeeJihun <ljh09060@gmail.com>
2025-06-10 11:10:26 +0900

    feat: Add new solutions

---
+++ project/07/main.h
@@ -0,0 +1,19 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <iostream>
+#include <cstring>
+
+#define MAX_CUSTOMER_CNT 50
+#define MAX_NAME_LEN 20
+
+using namespace std;
+
+enum CREDITLEVEL {
+	A = 7,
+	B = 4,
+	C = 2,
+};
+
+
+#endif
