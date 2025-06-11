068a0f71e1a8890f461978aa8c2fbf55edc7a6da
LeeJihun <ljh09060@gmail.com>
2025-06-11 16:03:05 +0900

    feat: Add new solutions

---
+++ 11/examples/ex11-1-1.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Gun {
+private:
+	int bullet;
+public:
+	Gun(int bnum) : bullet(bnum) {
+	}
+	void Shot() {
+		cout << "BBANG!" << endl;
+		bullet--;
+	}
+};
+
+class Police {
+private:
+	int handcuffs;
+	Gun *pistol;
+public:
+	Police(int bnum, int bcuff)
+		: handcuffs(bcuff) {
+		if (bnum > 0) {
+			pistol = new Gun(bnum);
+		}
+		else {
+			pistol = NULL;
+		}
+	}
+
+	Police(const Police &copy) : handcuffs(copy.handcuffs) {
+		if (copy.pistol != NULL) {
+			pistol = new Gun(*(copy.pistol));
+		}
+		else {
+			pistol = NULL;
+		}
+	}
+
+	Police &operator=(const Police &ref) {
+		if (pistol != NULL) {
+			delete pistol;
+		}
+		if(ref.pistol != NULL) {
+			pistol = new Gun(*(ref.pistol));
+		}
+		handcuffs = ref.handcuffs;
+		return *this;
+	}
+
+	void PutHandcuff() {
+		cout << "SNAP!" << endl;
+		handcuffs--;
+	}
+	void Shot() {
+		if (pistol == NULL) {
+			cout << "Hut BBANG!" << endl;
+		}
+		else {
+			pistol->Shot();
+		}
+	}
+	~Police() {
+		if (pistol != NULL) {
+			delete pistol;
+		}
+	}
+};
+
+int main(void) {
+	Police pman1(5, 3);
+	Police pman2 = pman1;
+	pman2.Shot();
+	pman2.PutHandcuff();
+
+	Police pman3(2, 4);
+	pman3 = pman1;
+	pman3.Shot();
+	pman3.PutHandcuff();
+
+	return 0;
+}
